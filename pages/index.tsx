f6277e6eab262dc981985ceec7400894f918c514
mlawingco <62869210+mlawingco@users.noreply.github.com>
2021-08-07 21:47:53 +0800

    feat(open-graph): add open graph meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,18 @@ const Portfolio = (): JSX.Element => {
           name="description"
           content="My personal solace place on web-earth."
         />
+        <meta property="og:title" content="KL Lawingco" />
+        <meta property="og:site_name" content="Marc Jhon Lawingco" />
+        <meta property="og:url" content="https://kllawingco.netlify.app/" />
+        <meta
+          property="og:description"
+          content="Ohh you found me?. Howdy! I am Marc Jhon Lawingco. Or you could call me KL. That works too . . . I am a Software Engineer"
+        />
+        <meta property="og:type" content="profile" />
+        <meta
+          property="og:image"
+          content="https://kllawingco.netlify.app/KL_avatar.png"
+        ></meta>
       </Head>
       <Menu />
       <Grid
