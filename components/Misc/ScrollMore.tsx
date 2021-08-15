d77e2ad67672ca8bf068575dd47013b1cf70750f
mlawingco <62869210+mlawingco@users.noreply.github.com>
2021-08-15 13:24:36 +0800

    scroll up

--- components/Misc/ScrollMore.tsx
+++ components/Misc/ScrollMore.tsx
@@ -1,4 +1,10 @@
-import { Box, Icon, useBreakpointValue } from '@chakra-ui/react'
+import {
+  Box,
+  Icon,
+  Text,
+  useBreakpointValue,
+  useColorModeValue,
+} from '@chakra-ui/react'
 import { RiMouseLine } from 'react-icons/ri'
 import { motion, Variants, AnimatePresence } from 'framer-motion'
 import useScrollDirection, { ScrollDirection } from 'hooks/useScrollDirection'
@@ -23,20 +29,42 @@ const scrollMoreVariants: Variants = {
       duration: 1.6,
       ease: 'easeInOut',
       loop: Infinity,
-      delay: 2,
     },
+  },
+  // exit: {
+  //   opacity: [1, 0],
+  //   transition: {
+  //     duration: 0.5,
+  //     ease: 'easeOut',
+  //   },
+  // },
+}
+
+const emailVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 250,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
   },
   exit: {
     opacity: [1, 0],
+    y: [0, 250],
     transition: {
       duration: 0.5,
       ease: 'easeOut',
     },
   },
 }
+
 const ScrollMore = () => {
   const isMobile = useBreakpointValue(mobileBreakpointsMap)
   const scrollDirection = useScrollDirection(false, isMobile)
+  const emailColor = useColorModeValue('gray.800', 'gray.400')
+  const emailLine = useColorModeValue('teal.500', 'cyan.200')
+
   return (
     <Box
       position="fixed"
@@ -52,7 +80,6 @@ const ScrollMore = () => {
             initial="initial"
             animate={['hidden', 'bounce']}
             variants={scrollMoreVariants}
-            exit="exit"
           >
             <Icon
               w={6}
@@ -61,6 +88,53 @@ const ScrollMore = () => {
               color="currentColor"
               opacity="0.75"
             />
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {scrollDirection === ScrollDirection.Down && (
+          <motion.div
+            initial="hidden"
+            animate="show"
+            exit="exit"
+            variants={emailVariants}
+            whileHover={{ y: -50 }}
+          >
+            <Text
+              as="a"
+              paddingY={3}
+              href="mailto:marcjhon18@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              color={emailColor}
+              _hover={{
+                color: emailLine,
+                _after: {
+                  backgroundColor: 'cyan.200',
+                  opacity: 1,
+                },
+              }}
+              fontWeight="light"
+              fontSize="smaller"
+              position="relative"
+              letterSpacing={2}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              transform="rotate(90deg)"
+              marginBottom="5em"
+              _after={{
+                backgroundColor: emailLine,
+                height: '2px',
+                opacity: 0.5,
+                content: '""',
+                flex: 1,
+                width: { base: '5em', xl: '8em' },
+                marginLeft: '1em',
+              }}
+            >
+              marcjhon@gmail.com
+            </Text>
           </motion.div>
         )}
       </AnimatePresence>
